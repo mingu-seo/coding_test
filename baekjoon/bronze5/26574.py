c52ae616c6ebff91fda0566da03eed059c41927b
withsky-note <withsky999@gmail.com>
2024-06-06 09:05:55 +0900

    2024-06-05

---
+++ baekjoon/bronze5/26574.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+arr = []
+for _ in range(n) :
+    arr.append(input())
+
+
+for a in arr :
+    print(a, a)
---
+++ programmers/level2/P86971.java
@@ -0,0 +1,17 @@
+package level2;
+/*
+ * 전력망 둘로 나누기
+ * 
+ */
+public class P86971 {
+    public static void main(String[] args) {
+        P86971 p = new P86971();
+        int result = p.solution(9, new int[][]{{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}});
+        System.out.println(result);
+    }
+
+    public int solution(int n, int[][] wires) {
+        int answer = -1;
+        return answer;
+    }
+}
