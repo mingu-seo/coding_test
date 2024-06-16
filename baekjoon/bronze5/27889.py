3e95a73ed0de9a90eec8c31868f1689f924e470d
withsky-note <withsky999@gmail.com>
2024-06-17 08:56:02 +0900

    2024-06-17

---
+++ baekjoon/bronze5/27889.py
@@ -0,0 +1,7 @@
+school = {'NLCS':'North London Collegiate School',
+          'BHA':'Branksome Hall Asia',
+          'KIS':'Korea International School',
+          'SJA':'St. Johnsbury Academy'}
+
+name = input()
+print(school[name])
