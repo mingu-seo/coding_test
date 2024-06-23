989bc29e6a5031cbe48928d84460833596f99e34
withsky-note <withsky999@gmail.com>
2024-06-24 08:44:20 +0900

    2024-06-24

---
+++ baekjoon/bronze5/28927.py
@@ -0,0 +1,19 @@
+l1 = []
+l2 = []
+
+for v in map(int ,input().split()) :
+    l1.append(v)
+for v in map(int ,input().split()) :
+    l2.append(v)
+print(l1)
+print(l2)
+
+s1 = l1[0] * 3 + l1[1] * 20 + l1[2] * 120
+s2 = l2[0] * 3 + l2[1] * 20 + l2[2] * 120
+
+if s1 > s2 :
+    print('Max')
+elif s1 < s2 :
+    print('Mel')
+else :
+    print('Draw')
