1c0b75a029857721f52db221986fcdf8509c496b
withsky999@gmail.com <withsky999@gmail.com>
2024-09-05 10:07:07 +0900

    2024-09-05 B11283 P60058

---
+++ programmers/level2/P60058.java
@@ -0,0 +1,14 @@
+package level2;
+
+public class P60058 {
+    public static void main(String[] args) {
+        P60058 p = new P60058();
+        String result = p.solution("(()())()");
+        System.out.println(result);
+    }
+
+    public String solution(String p) {
+        String answer = "";
+        return answer;
+    }
+}
