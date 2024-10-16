eb4f2e5795fd0f845df351c3e95ba7dfda6f41eb
withsky999@gmail.com <withsky999@gmail.com>
2024-10-16 21:26:00 +0900

    2024-10-16 B17009

---
+++ baekjoon/bronze4/17009.py
@@ -0,0 +1,17 @@
+a1 = int(input())
+a2 = int(input())
+a3 = int(input())
+
+b1 = int(input())
+b2 = int(input())
+b3 = int(input())
+
+a = a1*3 + a2*2 + a3
+b = b1*3 + b2*2 + b3
+
+if a > b :
+    print('A')
+elif a < b :
+    print('B')
+else :
+    print('T')
