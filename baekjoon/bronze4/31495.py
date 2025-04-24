e4bb6c705d853059ea0136fd3d32b58785f06d39
withsky999@gmail.com <withsky999@gmail.com>
2025-04-24 22:21:25 +0900

    2025-04-24 B31495

---
+++ baekjoon/bronze4/31495.py
@@ -0,0 +1,14 @@
+line = input()
+
+# print(line.split('"'))
+cnt = line.count('"')
+if cnt < 2 or len(line) < 2:
+    print('CE')
+else :
+    s = line.index('"')
+    e = line.index('"', s+1)
+    word = line[s+1:e]
+    if word == line.replace('"', '') and word != '':
+        print(word)
+    else :
+        print('CE')
