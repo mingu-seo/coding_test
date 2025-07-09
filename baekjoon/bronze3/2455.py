d64b98e46638addfef4306793d0633605606a855
withsky999@gmail.com <withsky999@gmail.com>
2025-07-09 19:24:39 +0900

    2025-07-09 B2455

---
+++ baekjoon/bronze3/2455.py
@@ -0,0 +1,9 @@
+
+maxPerson = 0
+curPerson = 0
+for _ in range(4) :
+    d, u = map(int, input().split())
+    curPerson -= d
+    curPerson += u
+    if maxPerson < curPerson : maxPerson = curPerson
+print(maxPerson)
