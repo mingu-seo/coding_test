3f78ab243b2f01c760e21b90d05eeed64d542b7d
withsky999@gmail.com <withsky999@gmail.com>
2026-01-01 18:10:53 +0900

    2026-01-01 B34161

---
+++ baekjoon/bronze4/34161.py
@@ -0,0 +1,9 @@
+a = input()
+b = input()
+c = input()
+
+for a in range(1,11) :
+    for b in range (1,11) :
+        for c in range(1, 11) :
+            for d in range(1,11) :
+                print(-1)
