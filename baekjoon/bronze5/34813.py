daf48a030d1058501fbf38c798b3541a7fd8031e
withsky-note <withsky999@gmail.com>
2026-02-01 21:25:35 +0900

    2026-02-01 B34813

---
+++ baekjoon/bronze5/34813.py
@@ -0,0 +1,11 @@
+value = input()
+
+v = value[0]
+if v == 'F' :
+    print('Foundation')
+elif v == 'C' :
+    print('Claves')
+elif v == 'V' :
+    print('Veritas')
+elif v == 'E' :
+    print('Exploration')
