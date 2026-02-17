f9561e64f7fb46effe3a50328def2bf3717cb5e1
withsky-note <withsky999@gmail.com>
2026-02-17 15:42:16 +0900

    2026-02-17 B35097

---
+++ baekjoon/bronze4/35097.py
@@ -0,0 +1,13 @@
+result = []
+
+while True :
+    n = int(input())
+    if n == 0 : break
+    total = 0
+    for i in range(1, n+1) :
+        for j in range(1, n+1) :
+            total += i*j
+    result.append(total)
+
+for r in result :
+    print(r)
