eb80116e14944e6fad6f2660ea8e3c61624cdb95
withsky-note <withsky999@gmail.com>
2026-02-18 16:54:57 +0900

    2026-02-18 B34750

--- baekjoon/bronze4/34750.py
+++ baekjoon/bronze4/34750.py
@@ -1,15 +1,15 @@
-N = int(input())
+    N = int(input())
 
-parent = 0
+    parent = 0
 
-if N >= 1000000 :
-    parent = int(N * 0.2)
-elif N >= 500000 :
-    parent = int(N * 0.15)
-elif N >= 100000 :
-    parent = int(N * 0.1)
-else :
-    parent = int(N * 0.05)
+    if N >= 1000000 :
+        parent = int(N * 0.2)
+    elif N >= 500000 :
+        parent = int(N * 0.15)
+    elif N >= 100000 :
+        parent = int(N * 0.1)
+    else :
+        parent = int(N * 0.05)
 
-my = N - parent
-print(parent, my)
+    my = N - parent
+    print(parent, my)
